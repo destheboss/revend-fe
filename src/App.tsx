999928bd9e6693472d87d06eb6b5b0e0382f8dbf
Desi <kkk>
2024-03-27 22:09:21 +0100

    Created FE

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import UserList from './components/UserList'; // Adjust the path as needed
+
+function App() {
+  return (
+    <div className="App">
+      <UserList />
+    </div>
+  );
+}
+
+export default App;
